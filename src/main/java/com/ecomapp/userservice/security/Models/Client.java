8c751d82dfae13ee90d6657b0eb3bd51d6e42c51
sivaram045 <sivaram045@gmail.com>
2024-06-10 22:33:42 +0530

    signup working

---
+++ src/main/java/com/ecomapp/userservice/security/Models/Client.java
@@ -0,0 +1,14 @@
+package com.ecomapp.userservice.security.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+/*
+public class Client {
+}*/
+/*@Getter
+@Setter
+@Entity
+@Table(name = "client")*/
